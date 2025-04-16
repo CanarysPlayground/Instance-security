31564bcb70c575802992407e08119b496435f3c4
Sourav-Sarkar-ecanarys <sourav.sarkar@ecanarys.com>
2025-04-16 17:38:49 +0530

    Update ownerworkingorg.py

--- ownerworkingorg.py
+++ ownerworkingorg.py
@@ -10,7 +10,7 @@ import string
 import json
 from flask import Flask, request, render_template, jsonify
 
-# --- Config ---
+# --- Config --
 # IMPORTANT: Set these values directly for now, later move to environment variables
 GITHUB_TOKEN = ""  # Replace with your new token
 ENTERPRISE_ID = "E_kgDOAATOow"  # Your enterprise ID

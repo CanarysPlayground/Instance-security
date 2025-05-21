4a29deb5b6dec0d448a4ffd1c6d52a80c491d340
Sourav-Sarkar-ecanarys <sourav.sarkar@ecanarys.com>
2025-05-21 17:54:57 +0530

    Update LoggerExample.java

--- src/main/java/LoggerExample.java
+++ src/main/java/LoggerExample.java
@@ -1,12 +1,12 @@
-// package io.codecov;
+package io.codecov;
 
-// import org.slf4j.Logger;
-// import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-// public class LoggerExample {
-//     private static final Logger logger = LoggerFactory.getLogger(LoggerExample.class);
+public class LoggerExample {
+    private static final Logger logger = LoggerFactory.getLogger(LoggerExample.class);
 
-//     public void logSensitiveData(String password) {
-//         logger.info("User password: " + password);  // Unsafe logging
-//     }
-// }
+    public void logSensitiveData(String password) {
+        logger.info("User password: " + password);  // Unsafe logging
+    }
+}
